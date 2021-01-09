84d2721d4424b646a3b48c4c1e1b402a3621c758
Thanh Vu <thanhauco@gmail.com>
2021-01-09 06:41:00 -0800

    Add Check Endpoint

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from src.models import Transaction
+from src.rules import RuleEngine
 app = Flask(__name__)
-@app.route('/')
-def home(): return 'Fraud Detector'
+engine = RuleEngine()
+@app.route('/check', methods=['POST'])
+def check():
+    data = request.json
+    tx = Transaction(data['amount'], data['location'], data.get('user_id'))
+    is_fraud = engine.check(tx)
+    return jsonify({'fraud': is_fraud})
 if __name__ == '__main__': app.run()
