27c46510db70be4388e152ee667c27020cdd0b4a
Thanh Vu <thanhauco@gmail.com>
2021-01-23 03:40:00 -0800

    Implement training logic

--- train.py
+++ train.py
@@ -1,5 +1,15 @@
+import sqlite3
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 def train():
-    print('Training...')
+    conn = sqlite3.connect('fraud.db')
+    df = pd.read_sql('SELECT * FROM transactions', conn)
+    if len(df) < 10: return
+    X = df[['amount']]
+    y = df['is_fraud']
+    clf = RandomForestClassifier()
+    clf.fit(X, y)
+    with open('model.pkl', 'wb') as f: pickle.dump(clf, f)
+    print('Model trained')
+if __name__ == '__main__': train()
