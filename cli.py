39c8fdee54bd18c3cffb8aefcd81aa0e44411d65
Thanh Vu <thanhauco@gmail.com>
2021-02-14 21:26:00 -0800

    Add CLI help

--- cli.py
+++ cli.py
@@ -1,6 +1,9 @@
 import sys
 import requests
-if len(sys.argv) < 2: print('Usage: cli.py <amount>'); sys.exit(1)
+if len(sys.argv) < 2: 
+    print('Usage: cli.py <amount>')
+    print('Example: cli.py 100.50')
+    sys.exit(1)
 amount = float(sys.argv[1])
 resp = requests.post('http://localhost:5000/check', json={'amount': amount, 'location': 'CLI', 'user_id': 'admin'})
 print(resp.json())
