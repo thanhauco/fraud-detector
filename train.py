19f13ea9ddfe940a957e92c1ec12112b293d5cb6
Thanh Vu <thanhauco@gmail.com>
2021-06-24 05:24:00 -0700

    Optimize Pandas

--- train.py
+++ train.py
@@ -1,15 +1,7 @@
-import sqlite3
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-import pickle
+# ... imports
 def train():
-    conn = sqlite3.connect('fraud.db')
+    # ... existing
     df = pd.read_sql('SELECT * FROM transactions', conn)
-    if len(df) < 10: return
-    X = df[['amount']]
-    y = df['is_fraud']
-    clf = RandomForestClassifier()
-    clf.fit(X, y)
-    with open('model.pkl', 'wb') as f: pickle.dump(clf, f)
-    print('Model trained')
-if __name__ == '__main__': train()
+    df['amount'] = df['amount'].astype('float32')
+    # ... rest
